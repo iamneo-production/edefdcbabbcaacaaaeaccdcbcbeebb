3a02fe405ecfc67b4fe824f8534748d4707c100a
Iamneo <techadmin@iamneo.ai>
2023-10-05 13:18:09 +0000

    Backup Commit

---
+++ jdbcmaven/src/main/java/com.example.dao/DBConnection.java
@@ -0,0 +1,13 @@
+public class DBConnection {
+    	
+        public static Connection getConnection() throws Exception {        
+            ResourceBundle rb = ResourceBundle.getBundle("oracle");
+            String url = rb.getString("db.url");
+            String username = rb.getString("db.username");
+            String password = rb.getString("db.password");
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(url,username,password);       
+            return con;
+        }
+    }
+
--- jdbcmaven/src/main/java/com.example.dao/EmployeeDAO.java
+++ jdbcmaven/src/main/java/com.example.dao/EmployeeDAO.java
@@ -1,3 +1,18 @@
 public class EmployeeDAO {
-    
+    public List<Book> getAllUsers()throws Exception{
+		List<Book> userList = new ArrayList<>();				
+		Book book=null;
+		
+		Connection	con=DBConnection.getConnection();		
+		Statement st=con.createStatement();
+		//ResultSet rs=st.executeQuery("select *from \"user\"");
+		ResultSet rs=st.executeQuery("select *from book");
+		
+		while(rs.next()) {			
+			book=new Book(rs.getInt(1), rs.getString(3),rs.getString(2),rs.getDouble(4));
+			userList.add(book);			
+		}
+		return userList;
+	}
+	
 }
